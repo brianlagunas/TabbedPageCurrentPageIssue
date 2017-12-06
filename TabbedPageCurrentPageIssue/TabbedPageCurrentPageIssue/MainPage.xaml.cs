b9f427540e8fb818c555f69b589387d4743bf0ff
Brian Lagunas <brian.lagunas@live.com>
2017-12-06 15:29:34 -0700

    added projects

---
+++ TabbedPageCurrentPageIssue/TabbedPageCurrentPageIssue/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabbedPageCurrentPageIssue
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            CurrentPageChanged += MainPage_CurrentPageChanged;
+        }
+
+        private async void MainPage_CurrentPageChanged(object sender, EventArgs e)
+        {
+            if (CurrentPage != null)
+                await DisplayAlert("Tab Changed", CurrentPage.Title, "Close");
+        }
+    }
+}
---
+++ TabbedPageCurrentPageIssue/TabbedPageCurrentPageIssue/Tab.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedPageCurrentPageIssue
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Tab : ContentPage
+	{
+		public Tab ()
+		{
+			InitializeComponent ();
+            _btn.Clicked += _btn_Clicked;
+		}
+
+        private async void _btn_Clicked(object sender, EventArgs e)
+        {
+            var parent = this.Parent as TabbedPage;
+            if (parent?.CurrentPage != null)
+                await DisplayAlert("CurrentPage", Title, "Close");
+        }
+    }
+}
